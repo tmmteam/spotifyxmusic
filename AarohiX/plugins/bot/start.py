448b4269831071c2426c12b78eb3fb25ce5ea1bf
tmmteam <teamtmm2@gmail.com>
2024-08-18 19:42:36 +0530

    Update start.py

--- AarohiX/plugins/bot/start.py
+++ AarohiX/plugins/bot/start.py
@@ -29,13 +29,36 @@ from strings import get_string
 
 """
 ADISA_PICS = [
-"https://graph.org/file/e0c7e04cc0acff425fe5d.jpg",
-"https://graph.org/file/0f256eac6f4a8a053193a.jpg",
-"https://graph.org/file/e3babc6e511746be05204.jpg",
-"https://graph.org/file/323acfeef219c919091ec.jpg",
-"https://graph.org/file/1f34f63999a5599051b94.jpg",
-"https://graph.org/file/528b50c44cbfedda9c77e.jpg",
-"https://graph.org/file/365b3ab63ccd789f99bb4.jpg"
+"https://graph.org/file/9bba2b7ee9ba3806de65d.jpg",
+    "https://graph.org/file/ef82f289043a4fa74f8ff.jpg",
+    "https://graph.org/file/9c27c68958e06ae074c38.jpg",
+    "https://graph.org/file/0ff325b1d2efe80299aa3.jpg",
+    "https://graph.org/file/41167b953cf3579853d47.jpg",
+    "https://graph.org/file/bd93ab42e69305f274028.jpg",
+    "https://graph.org/file/97575db5586c05d6b2898.jpg",
+    "https://graph.org/file/07c393fdf931a407c9bc0.jpg",
+    "https://graph.org/file/f332767490ad3a5ca20e8.jpg",
+    "https://graph.org/file/f3449e9069667f647d14e.jpg",
+    "https://graph.org/file/9f51cdc739f907cbd2c7e.jpg",
+    "https://telegra.ph/file/d7a6a923c38e051ce35f3.jpg",
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg",
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",    
 ]
 
 
@@ -51,7 +74,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = first_page(_)
-            await message.reply_sticker("CAACAgUAAx0CcSyO9gACl7dkQoL0p1vXqcYtCuCHmSyh4Hi1UgACEwgAAonAEVYwaJ0Jkk8L0B4E")
+            await message.reply_sticker("CAACAgUAAx0CfLoeOQACaZVmIIOAjVdqAXMIZRhQ3uselOvotQAC4RAAAkzCcVSCoM-aLK5MsR4E")
             return await message.reply_photo(
                 photo=config.START_IMG_URL,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
@@ -104,7 +127,7 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
-        await message.reply_sticker("CAACAgUAAx0CcSyO9gACl7dkQoL0p1vXqcYtCuCHmSyh4Hi1UgACEwgAAonAEVYwaJ0Jkk8L0B4E")
+        await message.reply_sticker("CAACAgUAAx0CfLoeOQACaZVmIIOAjVdqAXMIZRhQ3uselOvotQAC4RAAAkzCcVSCoM-aLK5MsR4E")
         await message.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
