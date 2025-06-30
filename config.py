162e8a25919dc7918a58510dbc1e0ce4230085ca
KALI-OFFICIAL1 <kaliofficial448@gmail.com>
2025-06-30 09:50:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "54000")
 )
 
-LOGGER_ID = int(getenv("LOGGER_ID", ""))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002106071049"))
 
 OWNER_ID = int(getenv("OWNER_ID", "5311223486"))
 
